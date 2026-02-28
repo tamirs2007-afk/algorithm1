5e1be28d18d5af7206e059b7a85814e6b253139b
Tamir <tamir.s.2007@gmail.com>
2026-02-28 15:54:54 +1000

    zadacha1

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
-print("Second commit1")
-print("Second commit21")
+def intersection(arr1, arr2):
+    return list(set(arr1) & set(arr2))
+
+print(intersection([1, 2, 2, 3], [2, 3, 4] ))
+print(intersection([1, 2], [ 3, 4] ))
+print(intersection([], [ 1, 2] ))
