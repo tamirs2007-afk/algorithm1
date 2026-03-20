a8004afef331310e645b1c26330578edc6e48603
Tamir <tamir.s.2007@gmail.com>
2026-03-20 16:48:11 +1000

    Hello world

--- main.py
+++ main.py
@@ -1,6 +1,1 @@
-def intersection(arr1, arr2):
-    return list(set(arr1) & set(arr2))
-
-print(intersection([1, 2, 2, 3], [2, 3, 4] ))
-print(intersection([1, 2], [ 3, 4] ))
-print(intersection([], [ 1, 2] ))
+print('Hello world')
